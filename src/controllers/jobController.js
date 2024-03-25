90ebf82ada2b6872144e0cedb1f4c40180eff58e
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2024-03-25 19:08:48 +0800

    added job controller

---
+++ src/controllers/jobController.js
@@ -0,0 +1,19 @@
+export const getAllJobs = async (req, res) => {
+  res.send("Get All Jobs Route");
+};
+
+export const getJob = async (req, res) => {
+  res.send("Get Single Job Router");
+};
+
+export const createJob = async (req, res) => {
+  res.send("Create Job Route");
+};
+
+export const updateJob = async (req, res) => {
+  res.send("Update Job Route");
+};
+
+export const deleteJob = async (req, res) => {
+  res.send("Delete Job Route");
+};
