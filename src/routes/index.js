499d5c2c61e29505ea968fd843a12142d02ed173
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2024-03-27 11:38:42 +0800

    added verify token middleware in job route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,12 @@ import { Router } from "express";
 import authRoute from "./authRoute.js";
 import jobRoute from "./jobRoute.js";
 
+/** IMPORT: MIDDLEWARE */
+import verifyToken from "../middleware/verifyToken.js";
+
 const router = Router();
 
 router.use("/auth", authRoute);
-router.use("/jobs", jobRoute);
+router.use("/jobs", verifyToken, jobRoute);
 
 export default router;
