158acff4d3b0261e4eea153977e6eda6a0a7cf3b
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2024-03-30 19:11:25 +0800

    modified the user schema

--- src/models/User.js
+++ src/models/User.js
@@ -3,11 +3,32 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema({
-  name: {
+  firstName: {
     type: String,
-    required: [true, "Please provide a name"],
-    minlength: 3,
-    maxlength: 50,
+    trim: true,
+    maxlength: 20,
+    default: "",
+  },
+  middleName: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "",
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "",
+  },
+  fullName: {
+    type: String,
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "",
   },
   email: {
     type: String,
@@ -31,9 +52,31 @@ UserSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
+// ! fullName must not be required if you want to populate it before saving
+UserSchema.pre("save", function (next) {
+  const { firstName, middleName, lastName } = this;
+  this.fullName = `${firstName} ${middleName} ${lastName}`;
+  next();
+});
+
+UserSchema.pre("findOneAndUpdate", function (next) {
+  // Check if any of the name fields have been modified
+  if (
+    this._update.firstName ||
+    this._update.middleName ||
+    this._update.lastName
+  ) {
+    // Populate fullName based on updated values
+    this._update.fullName = `${this._update.firstName || this._doc.firstName} ${
+      this._update.middleName || this._doc.middleName
+    } ${this._update.lastName || this._doc.lastName}`;
+  }
+  next();
+});
+
 UserSchema.methods.accessToken = function () {
   return jwt.sign(
-    { userId: this._id, name: this.name },
+    { userId: this._id, fullName: this.fullName },
     process.env.JWT_SECRET,
     { expiresIn: process.env.JWT_LIFETIME }
   );
